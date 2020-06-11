e6d9329dbbbf9687d7da9d959b10bc77b1283d40
TianYujiaEureka <32934461+TianYujiaEureka@users.noreply.github.com>
2020-06-11 10:23:49 +0800

    Add files via upload

--- USER/main.c
+++ USER/main.c
@@ -10,14 +10,14 @@
 #include "LED_DISPLAY.h"
 #include "sys.h"
 #include "USB_To_Usart.h"	
-	void GetSHTDataToGlobParams(int num)
+	void GetSHTDataToGlobParams(uint32_t* sendData)
 {
 	int sendDataLen=0;
 	int sendDataLen1=0;
 	
 	u8 	buffer[128];
 	uint8_t 	ServerUartBufferTx[128];
-	uint32_t sendData[3];
+	
 	u16 humi_val, temp_val;
 	u8 err = 0, checksum = 0;
 	float humi_val_real = 0.0; 
@@ -36,16 +36,17 @@
 			OLED_ShowString(0,38, buffer,12);
 			
 			sendData[0]=((int)temp_val_real<<8)+(int)(temp_val_real*10)%10;
-			sendData[1]=((int)humi_val_real<<8)+(int)(humi_val_real*10)%10;			
-			sendData[2]=num;
+			sendData[1]=((int)humi_val_real<<8)+(int)(humi_val_real*10)%10;		
 			sendDataLen=getData(ServerUartBufferTx,&sendDataLen1,sendData,3);
 			if(!getLinkStatus()){
 				E103_W02_SendData(ServerUartBufferTx,sendDataLen);
 			}
 	
 }
+
 int main(void)
 { int num=0;
+	uint32_t sendData[3];
 	u8 	buffer[128];
   u8 key;           //保存键值
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
@@ -56,7 +57,7 @@ int main(void)
 	uart_init(115200);
 	SHT11_Config();
 	init_E103_w02();//初始化WIFI
-	initDataFrame(0xDC201001661CBFC2,0x123456);//初始化数据协议，并设置协议中用到的设备id和登录密码
+	initDataFrame(0xDC20100166146533,0x123456);//初始化数据协议，并设置协议中用到的设备id和登录密码
 	LED_DISPLAY_Init();//初始化数码管I/O
 	OLED_ShowString(0,0, (unsigned char *)"KEY_UP: Link_Server",12);
 	OLED_ShowString(0,14, (unsigned char *)"KEY_1 : Rest_WIFI",12);
@@ -71,15 +72,22 @@ int main(void)
 	{	if(WIFI_USART_RX_STA>0x8000){
 				//SendDataToDevice_a(WIFI_USART_RX_BUF, WIFI_USART_RX_STA^0x8000);//打印指令包
 				delay_ms(11);
-				OLED_ShowString(0,50, (unsigned char *)" >80000  ",12);
 				if(ParseServerData(WIFI_USART_RX_BUF, WIFI_USART_RX_STA^0x8000)){
-				OLED_ShowString(0,50, (unsigned char *)"……&……80000 ",12);
-					if(dataFrame.mOrderConnect[0]==1){
+					if(dataFrame.mOrderConnect[0]<=1){
+						if(dataFrame.mOrderConnect[0]==0){
 						LED0=1;
-						OLED_ShowString(0,50, (unsigned char *)"    %1%      ",12);
-					}else{
+						LED0=1;
+						OLED_ShowString(0,50, (unsigned char *)"   admin state   ",12);
+					}
+						else{LED1=0;
+						LED0=1;
+						OLED_ShowString(0,50, (unsigned char *)"   already pay   ",12);
+						}
+					}
+					else{
 						LED0=0;
-						OLED_ShowString(0,50, (unsigned char *)"     %0%   ",12);
+						LED1=1;
+						OLED_ShowString(0,50, (unsigned char *)"   not pay yet   ",12);
 					}
 					sprintf((char *)buffer,"指令数值:%d\r\n", dataFrame.mOrderConnect[0]);
 					SendDataToDevice_a(buffer, 20);
@@ -106,7 +114,7 @@ int main(void)
 				
 					break;
 				case KEY1_PRES:	//恢复出厂化设置
-					GetSHTDataToGlobParams(-1);
+					sendData[2]=0;
 					//SendDataToDevice_a("重置",4);
 					delay_ms(100);	
 					OLED_ShowString(0,50, (unsigned char *)"WIFI Rest start...",12);	
@@ -120,14 +128,15 @@ int main(void)
 				
 					break;
 				case KEY0_PRES:
-					OLED_ShowString(0,50, (unsigned char *)" add NUM ING   ",12);
+					OLED_ShowString(0,50, (unsigned char *)"   add num ing   ",12);
 					num++;
 					num=num%4;
 					break;
 				case KEY2_PRES:	//
-					LED0=1;
-					OLED_ShowString(0,50, (unsigned char *)"SEND DATA",12);
-					
+					LED0=0;
+					LED1=0;
+					OLED_ShowString(0,50, (unsigned char *)"   wait for pay   ",12);
+					sendData[2]=num;
 					break;
 			}show_num(num);
 		}else{ delay_ms(10); }
@@ -137,7 +146,8 @@ int main(void)
 		}else{
 			OLED_ShowString(6*11,26, (unsigned char *)"OK",12);
 		}
-		GetSHTDataToGlobParams(num);		
+		
+		GetSHTDataToGlobParams(sendData);		
 		OLED_Refresh_Gram();//更新显示到OLED	 	
 	}
 }
